6656f9acf6991b85bdeceb155eae783c0c0bafd8
ersin sivaz <ersinsivaz@hotmail.com>
2017-12-21 22:02:13 +0300

    package

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CustomerListComponent } from './customer-list/customer-list.component';
-import { CustomerFormComponent } from './customer-form/customer-form.component';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CustomerListComponent, CustomerFormComponent]
+  declarations: []
 })
 export class CustomerModule { }
