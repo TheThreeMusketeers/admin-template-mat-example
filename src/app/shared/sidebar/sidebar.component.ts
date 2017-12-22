30d5db7f62c25325e27abbc3e6f0e357018e899b
ersin sivaz <ersinsivaz@hotmail.com>
2017-12-22 15:41:41 +0300

    sidenav escape düzeltme

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent  {
   showFiller = false;
   
   constructor() { }
+  
+  @ViewChild('sidenav') sidenav: MatSidenav;
+  
+    reason = '';
+  
+    close(reason: string) {
+      this.reason = reason;
+      this.sidenav.close();
+    }
 
-  ngOnInit() {
-  }
 
 }
